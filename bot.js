1d920776c5c6eb423908c41ace5fb829b8b96291
Michael C <michael@mchang.name>
2021-04-04 00:52:37 -0400

    reduced lines

--- bot.js
+++ bot.js
@@ -1,61 +1,31 @@
-// imports
 require('dotenv').config()
 const Discord = require('discord.js')
-const { exec, spawn } = require("child_process")
+const { exec } = require("child_process")
 
 const client = new Discord.Client() // create client
 client.on('ready', () => {
   console.log('Ready')
-  client.user.setPresence({ // set presence
-    activity: { type: process.env.ACT_TYPE, name: process.env.ACT_NAME },
-    status: process.env.STATUS
+  client.user.setPresence({
+    activity: { type: process.env.ACT_TYPE, name: process.env.ACT_NAME }, status: process.env.STATUS
   })
 })
-client.login(process.env.TOKEN) // login
+client.login(process.env.TOKEN)
+
+function execute(command, channel) {
+  exec(command, (err, stdout, stderr) => {
+    if (err) return channel.send(`error: ${err.message}`)
+    if (stderr) return channel.send(`stderr: ${stderr}`)
+    channel.send(`\`\`\`${stdout}\`\`\``)
+  })
+}
 
 client.on('message', message => {
-  const prefix = process.env.PREFIX // set prefix
-  if (!message.author.bot && message.content.startsWith(prefix)) { // check if sent by self & check for prefix
+  const prefix = process.env.PREFIX 
+  if (!message.author.bot && message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase()
-    // run appropiate command
-    if (command === 'wol') { // set map
-        exec(`wol ${process.env.MAC}`, (error, stdout, stderr) => {
-            if (error) {
-                message.channel.send(`error: ${error.message}`)
-                return;
-            }
-            if (stderr) {
-                message.channel.send(`stderr: ${stderr}`)
-                return
-            }
-            message.channel.send(`\`\`\`${stdout}\`\`\``)
-        })
-    } else if (command === 'ip') {
-        exec("ip -4 a", (error, stdout, stderr) => {
-            if (error) {
-                message.channel.send(`error: ${error.message}`)
-                return
-            }
-            if (stderr) {
-                message.channel.send(`stderr: ${stderr}`)
-                return
-            }
-            message.channel.send(`\`\`\`${stdout}\`\`\``)
-        }); 
-    } else if (command === 'ping') {
-        exec(`ping -c 5 ${process.env.IP}`, (error, stdout, stderr) => {
-            if (error) {
-                message.channel.send(`error: ${error.message}`)
-                return
-            }
-            if (stderr) {
-                message.channel.send(`stderr: ${stderr}`)
-                return
-            }
-            message.channel.send(`\`\`\`${stdout}\`\`\``)
-        }); 
-    } else { 
-        message.channel.send('wol | ip | ping')
-    }
+    if (command === 'wol') execute(`wol ${process.env.MAC}`, message.channel)
+    else if (command === 'ip') execute("ip -4 a", message.channel)
+    else if (command === 'ping') execute(`ping -c 5 ${process.env.IP}`, message.channel)
+    else message.channel.send('wol | ip | ping')
 }})
